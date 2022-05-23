1f7492da9a7fc51cf858e5a762b9ed98bf860d06
hunting945 <hunting945@livemail.tw>
2022-05-23 16:14:37 +0800

    add navigation on tap

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import {View, Text, StyleSheet, Button, FlatList, TouchableOpacity } from 'react
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
     const { state, addBlogPost, deleteBlogPost } = useContext(Context);
     
     return (
@@ -14,6 +14,7 @@ const IndexScreen = () => {
                 keyExtractor={blogPost => blogPost.title} 
                 renderItem={({item}) => {
                     return (
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
                         <View style={styles.row}>
                             <Text style={styles.title}>
                                 {item.title} - {item.id}
@@ -22,6 +23,7 @@ const IndexScreen = () => {
                                 <Feather style={styles.icon} name="trash" />
                             </TouchableOpacity>
                         </View>
+                        </TouchableOpacity>
                     );
                 }} 
             />
